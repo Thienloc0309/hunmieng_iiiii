5759a761c86909bfe79eaaa53085451bd3275614
Thienloc2715 <loc.tranthien3905@hcmut.edu.vn>
2025-11-20 09:39:39 +0700

    chinh sua

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,20 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "Thôi mà cho i",
+      "si nghĩ lại i",
+      "thoi mò cho đi mò",
+      "si nghĩ lại iiiiiiii",
+      "thôi hun 1 cái thoi",
+      "1 cái thoi mò",
+      "cho i :((",
+      "sầu qué à",
+      "plsssssssssss",
+      "thoi mà thương thương",
+      "hun 0.5 cái thoi",
       ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
+      "đi mò",
+      "cho iiiiiiiiiiiiiiiiiiiiiii",
       "No :(",
     ];
 
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">quá tuỵt zời hun chụt chụt chụt chụy chuutjtt chutjttuttjj chụttttttt ;))</div>
         </>
       ) : (
         <>
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Cho hun miếng iiiiiiiiiii</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
